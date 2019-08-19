dc8fbbc8e355721bd655355b35d96c95af06bb11
1196688913 <1196688913@qq.com>
2019-08-19 15:14:22 +0800

    fg

---
+++ Pages/numen/首页.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _8._18.Pages
+{
+    public class 首页Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
